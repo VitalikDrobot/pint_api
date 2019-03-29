fb636a43c8ebe42eb198214f2a486320403465a8
vitalikdrobot <vitalik@active-bridge.com>
2019-03-29 17:18:09 +0200

    Fixed connected options

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module PinterestApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'https://tranquil-crag-19897.herokuapp.com/'
         resource '*', headers: :any, methods: %i[get post put delete options]
       end
     end
